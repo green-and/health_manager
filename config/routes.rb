fe59469a6c5feeaa5c19a6dc65e4659560651ce6
green-and <t.nakamura.official@gmail.com>
2021-05-06 06:53:15 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'goals/index'
   devise_for :users
   root to: "goals#index"
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
