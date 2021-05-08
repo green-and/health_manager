988df01d52f0b0be57cbf93292574c874a83f945
green-and <t.nakamura.official@gmail.com>
2021-05-08 12:57:01 +0900

    profileデータ保存の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,17 @@
 class ProfilesController < ApplicationController
   def index
   end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    Profile.create(profile_params)
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:age, :height, :weight).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "profiles#index"
   resources :users, only: [:edit, :update]
-  resources :profiles, only: [:index]
+  resources :profiles, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
