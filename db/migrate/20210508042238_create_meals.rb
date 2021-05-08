5284d2fa1b897bb43b2e464ccd74e66d86865872
green-and <t.nakamura.official@gmail.com>
2021-05-08 15:30:05 +0900

    mealsテーブルの修正

--- db/migrate/20210508042238_create_meals.rb
+++ db/migrate/20210508042238_create_meals.rb
@@ -4,6 +4,7 @@ class CreateMeals < ActiveRecord::Migration[6.0]
       t.string :menu
       t.integer :wheat_id
       t.integer :calory
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
