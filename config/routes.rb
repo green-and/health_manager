2487644399b937ca50db516ec59b2a9078d54776
green-and <t.nakamura.official@gmail.com>
2021-05-08 14:37:43 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: "profiles#index"
   resources :users, only: [:edit, :update]
   resources :profiles, only: [:index, :new, :create]
+  resources :meals, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
