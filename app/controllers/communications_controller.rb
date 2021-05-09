4ed4bab4f45d4666d71d98badde2deef8c3a4c0c
green-and <t.nakamura.official@gmail.com>
2021-05-10 06:43:35 +0900

    データ保存の実装

--- app/controllers/communications_controller.rb
+++ app/controllers/communications_controller.rb
@@ -1,4 +1,18 @@
 class CommunicationsController < ApplicationController
   def index
+    @communication = Communication.new
+  end
+
+  def create
+    if Communication.create(communication_params)
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def communication_params
+    params.require(:communication).permit(:time).merge(user_id: current_user.id)
   end
 end
