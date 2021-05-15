d891ebe17de32004fcb2d42ec147852abbb1dd08
green-and <t.nakamura.official@gmail.com>
2021-05-16 08:34:38 +0900

    digital目標値と履歴の表示

--- app/controllers/digitals_controller.rb
+++ app/controllers/digitals_controller.rb
@@ -1,6 +1,8 @@
 class DigitalsController < ApplicationController
   def index
     @digital = Digital.new
+    @goals = Goal.where(id: current_user.id)
+    @digitals = current_user.digitals
   end
 
   def create
