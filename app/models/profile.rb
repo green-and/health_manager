2ffbca8d9876916027a5eeda02debcb4b1b9e3e4
green-and <t.nakamura.official@gmail.com>
2021-06-03 07:02:41 +0900

    profileモデルの修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :wheatgoal
+
+  validates :wheatgoal_id, :calory_goal, :digital_goal, :communication_goal
 end
